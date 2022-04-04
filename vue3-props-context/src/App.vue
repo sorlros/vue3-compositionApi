3e056518740d563a7dac06dcc8755321994d405d
sorlros <sorlros2047@gmail.com>
2022-04-04 16:06:43 +0900

    props & context

---
+++ vue3-props-context/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <MyBtn 
+    class="sorlros"
+    style="color: red;"
+    color="#ff0000"
+    @hello="log">
+    Apple
+  </MyBtn>  
+</template>
+
+<script>
+import MyBtn from '~/components/MyBtn'
+
+export default {
+  components: {
+    MyBtn
+  },
+  methods: {
+    log() {
+      console.log('Hello World')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>
